c092917c9474fdd1c0c8671815fa70ae355651cd
Aaron <aeroyts@icloud.com>
2020-10-24 17:41:06 +0100

    Contact Page Added
    Contact page added along with relevant sass file additions

--- page-contact.php
+++ page-contact.php
@@ -5,7 +5,24 @@ the_post();
 
 ?>
 
-
+<div class="contact-form">
+    <div class="container text-center">
+        <h1>CONTACT US</h1>
+        <p>If you need to get in touch, enter your message here or message us in the Discord Group.</p>
+        <form>
+            <div class="form-group">
+                <input type="text" class="form-control" id="nameInput" placeholder="Your Name">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="discordID" placeholder="Your Discord ID">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="messageInput" placeholder="Your Message">
+            </div>
+        </form>
+        <a class="send-button">SEND</a>
+    </div>
+</div>
 
 <?php
 
